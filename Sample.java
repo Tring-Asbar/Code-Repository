5b3818743c0b306d462d20649d3b8fe567be2889
Tring-Asbar <asbar.j@tringapps.com>
2025-01-23 14:28:15 +0530

    Intro

--- Sample.java
+++ Sample.java
@@ -1,5 +1,6 @@
 public class Sample{
     public static void main(String[] args){
+        System.out.println("Myself Asbar");
         System.out.print("Hello!, Tringapps");
         System.out.println("Thanks for giving me this opportunity");
     }
